c6aba78e9219f36b506547978a7d66b059033f24
safonov_dmitriy <sdssds341@gmail.com>
2021-11-25 15:08:31 +0200

    [refactor] Select

--- client/src/components/UI/Select/Select.jsx
+++ client/src/components/UI/Select/Select.jsx
@@ -1,12 +1,45 @@
 import React from "react";
-
-const Select = ({ label, options, ...props }) => {
-  console.log(`props`, props);
+import { TextFieldWrapper } from "../Inputs/TextFieldWrapper";
+import "./Select.styles.scss";
+const Select = ({
+  label = "",
+  options = [],
+  field = { onChange: () => {} },
+  handler = () => {},
+  defaultPlaceholder,
+  disabled,
+  form,
+  ...props
+}) => {
+  const { onChange, ..._field } = field;
+  const onChangeHandler = (e) => {
+    onChange(e);
+    handler(e.target.value);
+  };
   return (
-    <>
-      <label children={label} />
-      <select></select>
-    </>
+    <TextFieldWrapper
+      label={label}
+      name={field.name}
+      errors={form.errors}
+      touched={form.touched}
+      submitCount={form.submitCount}
+    >
+      <select
+        disabled={disabled}
+        className={!_field?.value?.length ? "select select_disabled" : "select"}
+        {..._field}
+        onChange={onChangeHandler}
+      >
+        {defaultPlaceholder && (
+          <option value="" disabled children={defaultPlaceholder} />
+        )}
+        {options.map((option) => (
+          <option key={option.id} value={option.id}>
+            {option.name}
+          </option>
+        ))}
+      </select>
+    </TextFieldWrapper>
   );
 };
 
--- client/src/pages/Reviews/CreateNewReviewsModal/FormForComment/index.js
+++
@@ -1,1 +0,0 @@
-export { default as FormForComment } from "./FormForComment";
