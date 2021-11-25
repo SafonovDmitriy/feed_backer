40626716d7f7800632861c385533e584771d614c
safonov_dmitriy <sdssds341@gmail.com>
2021-11-25 15:15:14 +0200

    [feat] Notification to Error  fetchCitiesSaga

--- client/src/redux/actions/cityActions.js
+++ client/src/redux/actions/cityActions.js
@@ -1,6 +1,8 @@
 import { call, put, takeLatest } from "redux-saga/effects";
 import { fetchCityApi } from "../../api/httpService";
 import { FETCH_CITIES, SET_CITIES, SET_CITIES_LOADED } from "../actionTypes";
+import { showErrorMessage } from "../../helpers/showNotificationMessage";
+
 export const citySagaWorker = [takeLatest(FETCH_CITIES, fetchCitiesSaga)];
 
 export const fetchCitiesAction = (payload) => ({
