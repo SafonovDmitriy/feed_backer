190c6908f96eb33e7c6ac490b00abac74534c2f7
safonov_dmitriy <sdssds341@gmail.com>
2021-11-25 15:08:12 +0200

    [refactor] Modal

--- client/src/components/UI/Modal/Modal.jsx
+++ client/src/components/UI/Modal/Modal.jsx
@@ -2,16 +2,13 @@ import React from "react";
 import { Button } from "../Button";
 import "./Modal.styles.scss";
 const Modal = ({ children, headerTitle, onClose, ...props }) => {
-  const onCloseModalWindow = (e) => {
-    if (e.target.id === "modal") onClose();
-  };
   return (
-    <div className="modal" id="modal" onClick={onCloseModalWindow}>
+    <div className="modal">
       <div className="modal_children">
         <div className="modal_children_header">
           {headerTitle}
           <Button
-            type="close"
+            typeStyles="close"
             classes={["modal_children_header_btn"]}
             onClick={onClose}
           />
