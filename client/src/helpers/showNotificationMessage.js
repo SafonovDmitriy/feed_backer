ba5075b9ea6adc188007e75d62b40485bb4a6dd9
safonov_dmitriy <sdssds341@gmail.com>
2021-11-25 15:10:51 +0200

    [feat] Notification Messages

---
+++ client/src/helpers/showNotificationMessage.js
@@ -0,0 +1,44 @@
+import { store as notificationStore } from "react-notifications-component";
+const TIME_FOR_NOTIFICATION = 2000;
+const congigForNotification = {
+  insert: "bottom",
+  container: "bottom-right",
+  animationIn: ["animate__animated", "animate__fadeIn"],
+  animationOut: ["animate__animated", "animate__fadeOut"],
+};
+const dismissForNotification = {
+  pauseOnHover: true,
+  timingFunction: "ease-out",
+  onScreen: true,
+};
+export const showSuccessMessage = (
+  message = "",
+  customeDuration = TIME_FOR_NOTIFICATION
+) => {
+  notificationStore.addNotification({
+    title: "Success!",
+    message,
+    type: "success",
+    ...congigForNotification,
+    dismiss: {
+      duration: customeDuration,
+      ...dismissForNotification,
+    },
+  });
+};
+
+export const showErrorMessage = (
+  message = "",
+  customeDuration = TIME_FOR_NOTIFICATION
+) => {
+  notificationStore.addNotification({
+    title: "Error",
+    message,
+    type: "danger",
+    ...congigForNotification,
+    dismiss: {
+      duration: customeDuration,
+      ...dismissForNotification,
+    },
+  });
+};
