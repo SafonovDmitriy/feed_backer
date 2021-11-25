069cb36591f2a82cc0dbac000398a91e392f7a2a
safonov_dmitriy <sdssds341@gmail.com>
2021-11-25 15:09:32 +0200

    [feat] selectors

--- client/src/redux/selectors/index.js
+++ client/src/redux/selectors/index.js
@@ -1,8 +1,10 @@
 export const citiesSelector = (state) => state.city.data;
+export const isEmptyCitiesSelector = (state) => !state.city.data.length;
 export const citiesLoadedSelector = (state) => state.city.loaded;
 
 export const feedbacksSelector = (state) => state.feedback.data;
 export const feedbacksLoadedSelector = (state) => state.feedback.loaded;
 
 export const regionsSelector = (state) => state.region.data;
+export const isEmptyRegionsSelector = (state) => !state.region.data.length;
 export const regionsLoadedSelector = (state) => state.region.loaded;
