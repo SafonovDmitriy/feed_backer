62a7e594fbff4b42dccbbf035a38997d3cbf80b9
safonov_dmitriy <sdssds341@gmail.com>
2021-11-25 15:03:25 +0200

    [feat] VALIDATION_MESSAGES_MAP to constants

--- client/src/constants.js
+++ client/src/constants.js
@@ -2,3 +2,8 @@ export const ROUTING_MAP = {
   REVIEWS: "/reviews",
   REPORT: "/report",
 };
+export const VALIDATION_MESSAGES_MAP = {
+  required: "Обязательное поле",
+  length: "Некорректный длина номера",
+  invalidEmail: "Некорректный email",
+};
