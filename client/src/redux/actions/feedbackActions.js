76520857357719cebe94815dd1f9b8e30b13beec
safonov_dmitriy <sdssds341@gmail.com>
2021-11-25 15:08:53 +0200

    [refactor] feedActions

--- client/src/redux/actions/feedbackActions.js
+++ client/src/redux/actions/feedbackActions.js
@@ -1,31 +1,87 @@
 import { call, put, select, takeLatest } from "redux-saga/effects";
 import {
+  deleteFeedbackApi,
+  fetchFeedbackApi,
+  postFeedbackApi,
+  putFeedbackApi,
+} from "../../api/httpService";
+import {
+  DELETE_FEEDBACK,
   FETCH_FEEDBACK,
+  POST_FEEDBACK,
+  PUT_FEEDBACK,
   SET_FEEDBACK,
   SET_FEEDBACK_LOADED,
 } from "../actionTypes";
+import { feedbacksSelector } from "../selectors";
 export const feedBackSagaWorker = [
   takeLatest(FETCH_FEEDBACK, fetchFeedBackSaga),
+  takeLatest(POST_FEEDBACK, postFeedBackSaga),
+  takeLatest(PUT_FEEDBACK, putFeedBackSaga),
+  takeLatest(DELETE_FEEDBACK, deleteFeedBackSaga),
 ];
 
 export const fetchFeedBackAction = () => ({
   type: FETCH_FEEDBACK,
 });
 
-export const setFeedBackAction = () => ({
-  type: SET_FEEDBACK,
+export const postFeedBackAction = (payload) => ({
+  type: POST_FEEDBACK,
+  payload,
+});
+export const putFeedBackAction = (payload) => ({
+  type: PUT_FEEDBACK,
+  payload,
+});
+export const deleteFeedBackAction = (payload) => ({
+  type: DELETE_FEEDBACK,
+  payload,
 });
 
-export const setLoadingFlagForFeedBackAction = (payload) => ({
+export const setFeedBackAction = (payload) => ({
+  type: SET_FEEDBACK,
+  payload,
+});
+
+export const setLoadedFlagForFeedBackAction = (payload) => ({
   type: SET_FEEDBACK_LOADED,
   payload,
 });
 
 function* fetchFeedBackSaga() {
   try {
-    // const {
-    //   data: { totalPage, users },
-    // } = yield call(getAllUsersApi, { id:payload });
+    yield put(setLoadedFlagForFeedBackAction(false));
+    const {
+      data: { data },
+    } = yield call(fetchFeedbackApi);
+    yield put(setFeedBackAction(data));
+  } catch ({ data, status }) {
+    // showErrorMessage(data?.message);
+  } finally {
+    yield put(setLoadedFlagForFeedBackAction(true));
+  }
+}
+function* postFeedBackSaga({ payload }) {
+  try {
+    yield call(postFeedbackApi, payload);
+    yield put(fetchFeedBackAction());
+  } catch ({ data, status }) {}
+}
+function* putFeedBackSaga({ payload }) {
+  try {
+    yield call(putFeedbackApi, payload);
+    yield put(fetchFeedBackAction());
+  } catch ({ data, status }) {}
+}
+function* deleteFeedBackSaga({ payload }) {
+  try {
+    const feedBackList = yield select(feedbacksSelector);
+    yield call(deleteFeedbackApi, payload);
+    yield put(
+      setFeedBackAction(
+        feedBackList.filter((feedBack) => feedBack.feedback_id !== payload)
+      )
+    );
     // yield put(setUsersAction(users));
     // yield put(setUsersTotalPageAction(totalPage));
   } catch ({ data, status }) {
