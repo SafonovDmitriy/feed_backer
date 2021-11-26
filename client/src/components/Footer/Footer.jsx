4f3bdce0ebd534390b15a3a186af8ce871b22eac
safonov_dmitriy <sdssds341@gmail.com>
2021-11-26 10:18:50 +0200

    [feat] Footer Component

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.styles.scss";
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <h5
+        className="footer_title"
+        children="FeedBacker (c) 2017 by Humster Elements Itd."
+      />
+    </footer>
+  );
+};
+export default Footer;
--- client/src/components/Router/Router.jsx
+++ client/src/components/Router/Router.jsx
@@ -1,15 +1,23 @@
 import React from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import { ROUTING_MAP } from "../../constants";
 import { Report } from "../../pages/Report";
 import { Reviews } from "../../pages/Reviews";
+import { Footer } from "../Footer";
+import "./Router.styles.scss";
 const Router = () => {
   return (
-    <Switch>
-      <Route exact path={ROUTING_MAP.REVIEWS} children={<Reviews />} />
-      <Route exact path={ROUTING_MAP.REPORT} children={<Report />} />
-      <Redirect to={ROUTING_MAP.REVIEWS} />
-    </Switch>
+    <div className="router">
+      <div className="router_content">
+        <Switch>
+          <Route exact path={ROUTING_MAP.REVIEWS} children={<Reviews />} />
+          <Route exact path={ROUTING_MAP.REPORT} children={<Report />} />
+          <Redirect to={ROUTING_MAP.REVIEWS} />
+        </Switch>
+      </div>
+
+      <Footer />
+    </div>
   );
 };
 export default Router;
